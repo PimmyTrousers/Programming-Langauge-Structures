247310f0e005f65cf477ba50af104f64ee12f8f2
Pim Trouerbach <pim.trouerbach1@gmail.com>
2017-09-21 23:01:12 -0700

    added stuff

--- parser.c
+++ parser.c
@@ -3,22 +3,31 @@
 #include <math.h>
 
 int is_it_a_decimal(double* result, char input_number[100]);
-
+int read_and_calculate_equation(double* result, char input_equation[100]);
 int main(int argc, char* argv[]){
   double number;
-  char input_number[100];
+  char input_equation[100];
   int result = 1;
+  FILE* file_pointer;
 
-  scanf("%s", input_number);
-
-  result = is_it_a_decimal(&number, input_number);
-
-  if(result){
-    printf("the input is %lf\n", number);
+  if(argc == 1){
+    printf("this program takes in a file as an arguement\n");
+    exit(0);
   }
-  else{
-    printf("what you inputted is not a number\n");
+  file_pointer = fopen(argv[1], "r");
+  if(file_pointer == NULL){
+    printf("something went wrong...");
+    exit(0);
   }
+
+  /* while(fscanf(file_pointer, "%c", input_equation) == 1){ */
+
+  /* } */
+
+  
+  char input_equation2[100] = "34 + 96 - 10 / 2";
+  read_and_calculate_equation(&number, input_equation2);
+
 }
 
 int is_it_a_decimal(double* result, char input_number[100]){
@@ -49,9 +58,11 @@ int is_it_a_decimal(double* result, char input_number[100]){
     if(input_number[i] > 32 && input_number[i] < 46){
       special_flag = 1;
       return 0;
-    }
+    } 
     total_number_length = i;
   }
+
+  // This is for doubles 
   if(period_flag && number_flag){
     int numbercount = 0;
     while(input_number[numbercount] != '.'){
@@ -72,5 +83,43 @@ int is_it_a_decimal(double* result, char input_number[100]){
     return 1;
   }
 
+  // This is for integers
+  if(number_flag){
+    double multiplier = 1;
+    for(int i = total_number_length; i != -1; i--){
+      *result = (input_number[i]-'0')*multiplier + *result;
+      multiplier = multiplier * 10;
+    }
+    return 1;
+  }
   return 0;
+}
+
+int read_and_calculate_equation(double* result, char input_equation[100]){
+  char temp_number[10];
+  int temp_counter = 0;
+  double temp_result = 0;
+  double total_result = 0;
+  int status_code = 1;
+
+  for(int i = 0; input_equation[i] != '\0'; ++i){
+    printf("currently looking at %c\n", input_equation[i]);
+    if(input_equation[i] == '+'){
+      printf("temp_number is %s\n", temp_number);
+      status_code = is_it_a_decimal(&temp_result, temp_number);
+      printf("the result from is it a decimal is %lf\n", *result);
+      if(status_code == 0){
+        printf("The equation you entered is wrong\n");
+        //exit(0);
+      }
+      for(int j = 0; j < 10; j++){
+        temp_number[j] = 0;
+      }
+    }
+      if(input_equation[i] == '-'){
+        printf("temp");
+      }
+    temp_number[temp_counter] = input_equation[i];
+  }
+  
 }
